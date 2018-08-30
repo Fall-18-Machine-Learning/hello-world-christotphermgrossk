d11eb56ab4427b032fe27c4d3ef98cfbdb296143
Christopher <christophermgrossk@lewisu.edu>
2018-08-30 11:20:22 -0500

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+#This is the HelloWorld.py program for week 1
+
+def helloWorld():
+    print("Hello World!")
+
+helloWorld()
---
+++ README.md
@@ -0,0 +1,5 @@
+#This is the ReadMe file for the Hello World program; Week 1.
+
+#Developer: Chris Grosskopf
+
+#Python 3.7
